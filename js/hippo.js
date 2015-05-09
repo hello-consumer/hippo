2e4d62cb6c59b140bf77968f368d4c78f360c8f8
arcoyk <sanhai.sato@gmail.com>
2015-05-10 03:53:58 +0900

    class

--- js/hippo.js
+++ js/hippo.js
@@ -1,13 +1,15 @@
-var hippo_item_list = ["corn",
-	  		"chocolate",
-	  		"andy",
-	  		"aramame",
-	  		"beans"];
+var item_list = ["corn",
+			"chocolate",
+			"andy",
+			"aramame",
+			"beans"];
 
-var hippo_item_matrix = [
+var item_matrix = [
 	  		[0, 0, 0, 0, 0],
 	  		[1, 4, 5, 6, 2],
 	  		[2, 4, 4, 5, 6],
 	  		[5, 3, 2, 4, 4],
 	  		[4, 2, 5, 2, 5]
 	  		];
+var data = {item_list:item_list,
+			item_matrix:item_matrix}
